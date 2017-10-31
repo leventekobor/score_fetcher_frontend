29135c53f6c8c526c87aa2f5cbab91f6eea70ff4
leventekobor94 <levente.kobor94@gmail.com>
2017-10-31 20:28:58 +0100

    init(gui.py)

--- src/external/gui.py
+++ src/external/gui.py
@@ -1,5 +1,16 @@
-import tkinter
+from tkinter import *
+from tkinter import messagebox
 
-top = tkinter.Tk()
+top = Tk()
+top.geometry("100x100")
+
+
+def refresh():
+	msg = messagebox.showinfo("Message" , "Refreshed!")
+
+
+B = Button(top, text = "Refresh", command = refresh)
+B.place(x = 50, y = 50)
+
 
 top.mainloop()
